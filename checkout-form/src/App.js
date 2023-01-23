1c414cfb6e0c89027326fcd1919760c0f3541ef3
Liam <nhblong1234@gmail.com>
2023-01-23 14:02:55 +0700

    dev

--- checkout-form/src/App.js
+++ checkout-form/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <button type="button" class="btn btn-primary">Checkout</button>
+      <Form name="Checkout form" />
     </div>
   );
 }
